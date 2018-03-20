b0cbd26cc5fc4ca67a35b4e48cc924b30972f2c8
Olendroza <34488056+Olendroza@users.noreply.github.com>
2018-03-20 17:19:50 +0100

    Joined postac with activity postac1 and added loops that meakes values of character appears in xml

--- app/src/main/java/com/example/piotr/generatorpostacimonastyr/postac1.java
+++ app/src/main/java/com/example/piotr/generatorpostacimonastyr/postac1.java
@@ -3,11 +3,12 @@ package com.example.piotr.generatorpostacimonastyr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class postac1 extends AppCompatActivity {
 
 
-    Postac postac;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +21,25 @@ public class postac1 extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        final Postac character = new Postac();
+        Button[] mainFactorValue =  new Button[8];
+        Button[] auxiliaryFactorValue = new Button[6];
+
+        for(int i=0;i<mainFactorValue.length;i++)
+        {
+            String source ="wspGValue"+Integer.toString(i+1);
+            int id = getResources().getIdentifier(source,"id",getPackageName());
+            mainFactorValue[i]=findViewById(id);
+            mainFactorValue[i].setText(Integer.toString(character.WspolczynnikiGlowne[i]));
+        }
+
+        for(int i=0;i<auxiliaryFactorValue.length;i++){
+            String source = "wspPValue"+Integer.toString(i+1);
+            int id = getResources().getIdentifier(source,"id",getPackageName());
+            auxiliaryFactorValue[i]=findViewById(id);
+            auxiliaryFactorValue[i].setText(Integer.toString(character.WspolczynnikiPomocnicze[i]));
+        }
+
     }
 
 
