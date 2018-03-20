bbc3032ebe394ffbb11aa02734691e13885d5aa3
Olendroza <34488056+Olendroza@users.noreply.github.com>
2018-03-20 16:35:39 +0100

    Added Postac class with basic constructor

--- app/src/main/java/com/example/piotr/generatorpostacimonastyr/postac1.java
+++ app/src/main/java/com/example/piotr/generatorpostacimonastyr/postac1.java
@@ -6,6 +6,8 @@ import android.view.View;
 
 public class postac1 extends AppCompatActivity {
 
+
+    Postac postac;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +20,6 @@ public class postac1 extends AppCompatActivity {
                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                         | View.SYSTEM_UI_FLAG_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
     }
 
 
